3d27ac9eaee19cb6f085bff75885b57071399794
imakjain <imakjain9@gmail.com>
2020-09-17 00:04:04 +0530

    login fixed

--- src/helpers/http-call.js
+++ src/helpers/http-call.js
@@ -1,70 +1,72 @@
 import "whatwg-fetch";
 
-let IP="18.191.204.2"
-let port=":8080"
+let IP = "18.191.204.2"
+let port = ":8080"
 
-let token = 'Bearer '+localStorage.getItem('token');
+let token = 'Bearer ' + localStorage.getItem('token');
 
 // let IP="localhost"
 // let port=":3500"
 
-export function getMethod(apiEndpoint,headers){
+export function getMethod(apiEndpoint, headers) {
     headers.Authorization = token;
-    return fetch("http://"+IP+port+apiEndpoint,{
+    return fetch("http://" + IP + port + apiEndpoint, {
         method: 'GET',
-        headers:headers,
+        headers: headers,
     })
-    .then( response =>{
-        return response.json();
-    })
-    .catch(error=>{
-        return Promise.reject(error);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            return Promise.reject(error);
+        })
 }
 
-export function putMethod(apiEndpoint,body,headers){
+export function putMethod(apiEndpoint, body, headers) {
     headers.Authorization = token;
-    return fetch("http://"+IP+port+apiEndpoint, {
+    return fetch("http://" + IP + port + apiEndpoint, {
         method: 'PUT',
-        headers:headers,
+        headers: headers,
         // credentials: 'include',
         body: JSON.stringify(body)
     })
-    .then( response =>{
-        return response.json();
-    })
-    .catch(error => {
-        return Promise.reject(error);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            return Promise.reject(error);
+        })
 }
 
-export function postMethod(apiEndpoint,body,headers){
-    headers.Authorization = token;
-    return fetch("http://"+IP+port+apiEndpoint, {
+export function postMethod(apiEndpoint, body, headers) {
+    if (apiEndpoint !== '/login') {
+        headers.Authorization = token;
+    }
+    return fetch("http://" + IP + port + apiEndpoint, {
         method: 'POST',
-        headers:headers,
+        headers: headers,
         // credentials: 'include',
         body: JSON.stringify(body)
     })
-    .then( response =>{
-        return response.json();
-    })
-    .catch(error=>{
-        return Promise.reject(error);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            return Promise.reject(error);
+        })
 }
 
-export function deleteMethod(apiEndpoint,headers){
+export function deleteMethod(apiEndpoint, headers) {
     headers.Authorization = token;
-    return fetch("http://"+IP+port+apiEndpoint,{
+    return fetch("http://" + IP + port + apiEndpoint, {
         method: 'DELETE',
-        headers:headers,
+        headers: headers,
     })
-    .then( response =>{
-        return response.json();
-    })
-    .catch(error => {
-        return Promise.reject(error);
-    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            return Promise.reject(error);
+        })
 }
 
